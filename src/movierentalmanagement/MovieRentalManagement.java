281f165d79236fb8740ff8bd56930da65c7825d9
Edow Jiru <agiantleapproduction@gmail.com>
2026-05-02 12:39:23 -0500

    Added main menu loop and DB connection setup

--- src/movierentalmanagement/MovieRentalManagement.java
+++ src/movierentalmanagement/MovieRentalManagement.java
@@ -4,6 +4,9 @@
  */
 package movierentalmanagement;
 
+
+import java.util.Scanner;
+import java.sql.Connection;
 /**
  *
  * @author georg
@@ -14,7 +17,18 @@ public class MovieRentalManagement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Scanner object to listen to user input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Grabs the database connection from DBManager 
+        Connection connection = DBManager.getConnection();
+        
+        // If the connection fails, prints message and exit immediately instead of crashing
+        if (connection == null) {
+            System.out.println("Failed to connect to database. Exiting...");
+            return;
+        }
     }
     
 }
